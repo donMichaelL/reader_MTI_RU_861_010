73821bcd92d24fa8c529c88b7843061590814363
Michael <michael.loukeris@icloud.com>
2016-08-23 14:50:41 +0300

    Adding status for antenna connectet/ not connected

--- status.py
+++ status.py
@@ -1,8 +1,13 @@
 import time
+import usb.core
 import json
 import paho.mqtt.publish as publish
+from subprocess import Popen, PIPE
+
 
 antenna_code = 0
+VENDOR_ID = '24e9'
+
 def get_antenna_code():
     global antenna_code
     if antenna_code == 0:
@@ -10,14 +15,27 @@ def get_antenna_code():
         antenna_code = file.read().rstrip()
     return antenna_code
 
+def findVendorProductId():
+    p = Popen('lsusb', shell=True, stdout=PIPE)
+    output, err = p.communicate()
+    start_of_vendor_id = output.find(str(VENDOR_ID))
+    if start_of_vendor_id != -1:
+        idProductStr = output[start_of_vendor_id+5:start_of_vendor_id+9]
+        idProduct = int('0x'+idProductStr, 16)
+        idVentor = int('0x'+VENDOR_ID, 16)
+        return (idVentor, idProduct)
 
 while 1:
     try:
-        print 'ok'
-        msg = json.dumps({'antenna': str(get_antenna_code())}, sort_keys=True,indent=4, separators=(',', ': '))
-        publish.single("input/" + get_antenna_code(), "Hello" , hostname="localhost", port=1883, client_id="",
-        keepalive=60, will=None, auth=None, tls=None)
-        print 'connected'
+        try:
+            idVendor, idProduct = findVendorProductId()
+            usb.core.find(idVendor=idVendor, idProduct=idProduct)
+            msg = json.dumps({'antenna': str(get_antenna_code()), 'status': 'connected'}, sort_keys=True, indent=4, separators=(',', ': '))
+            print 'connected'
+        except Exception as e:
+            msg = json.dumps({'antenna': str(get_antenna_code()), 'status': 'not connected'}, sort_keys=True, indent=4, separators=(',', ': '))
+            print 'not antenna'
+        publish.single("status", msg , hostname="localhost", port=1883, client_id="", keepalive=60, will=None, auth=None, tls=None)
     except Exception as e:
         print e, 'not connected'
     time.sleep(5)
